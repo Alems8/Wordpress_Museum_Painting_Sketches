a467e86446762529a4a231f0d3c977cf63fba566
Alems8 <90287909+Alems8@users.noreply.github.com>
2022-09-02 02:41:12 +0200

    Add files via upload

--- museum_theme/index.php
+++ museum_theme/index.php
@@ -1,29 +1,47 @@
-<?php get_header(); ?>
+<?php
+/*Template Name: Le opere*/
+get_header();
+?>
 
-<div class="content-homepage">
-    <?php
-        $home_query = new WP_query(
-                array(
-                        'post_status' => 'publish',
-                        'cat' => 4,
-                        'post_type' => 'post'
+<div class="content-main">
+	<?php
+	$page_id = get_queried_object_id();
 
-                )
-        );
-        if ($home_query->have_posts()){
-            while($home_query->have_posts()){
-                $home_query->the_post();
-        ?>
-        <h1><?php the_title();?></h1>
-        <p><?php the_content();?></p>
-        <?php
-         }
-        }
-    ?>
+	$home_query = get_posts(
+		array(
+			'post_type' => 'attachment',
+			'post_mime_type' => 'image',
+			'numberposts' => -1,
+			'post_status' => null,
+			'post_parent' => null
+		)
+	);
+
+	$query = new WP_Query(
+		array(
+			'post_type' => 'page',
+			'post_parent' => $page_id
+		)
+	);
+
+	if ($home_query && $query->have_posts()){
+		$url = get_page_link($query->post->ID);
+		foreach($home_query as $image){
+			$query->the_post();
+			?>
+
+            <div class="works">
+				<?php echo wp_get_attachment_image($image->ID, 'full'); ?>
+                <p><?php echo wp_trim_words(get_post($image->ID)->post_content, 20);
+					?>
+                    <a href="<?php echo $url?>?postID=<?php echo $image->ID; ?>">Continua a leggere</a> </p>
+            </div>
+			<?php
+		}
+	}
+	?>
 
 </div>
 
 
 <?php get_footer(); ?>
-
-
